29aca07aff0a9cfc9b77518a6a5396ec2b4cee26
SpirtCrazy <415131215@qq.com>
2016-12-17 00:59:34 +0800

    updateUI and debug

--- src/com/cqu/roy/mywdiget/JpathButton.java
+++ src/com/cqu/roy/mywdiget/JpathButton.java
@@ -3,14 +3,21 @@ package com.cqu.roy.mywdiget;
 import java.awt.Color;
 
 import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+
+import com.cqu.roy.fileOperation.newFile;
+
 
 public class JpathButton extends JButton{
 	private String MapFilePath;
+	
 	public JpathButton(String text,String path) {
 		// TODO Auto-generated constructor stub
 		super(text);
 		this.MapFilePath = path;
-		setBackground(Color.BLACK);
+		setBackground(new Color(50, 50, 50));
+		setFocusPainted(false);
 	}
 	
 	public void setMapFilePath(String mapFilePath) {
@@ -19,5 +26,10 @@ public class JpathButton extends JButton{
 	
 	public String getMapFilePath() {
 		return MapFilePath;
+	}
+	@Override
+	public void setText(String text) {
+		// TODO Auto-generated method stub
+		super.setText(text);
 	}
 }
