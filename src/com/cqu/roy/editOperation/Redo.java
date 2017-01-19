a91a39013a1475ad278c52b619944f8002cac1df
SpirtCrazy <415131215@qq.com>
2017-01-19 22:52:18 +0800

    redo undo

--- src/com/cqu/roy/editOperation/Redo.java
+++ src/com/cqu/roy/editOperation/Redo.java
@@ -2,20 +2,34 @@ package com.cqu.roy.editOperation;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Stack;
 import java.util.Vector;
 
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.StyledDocument;
 
 import com.cqu.roy.attribute.TextAtrr;
 import com.cqu.roy.fileOperation.FileOperation;
+import com.cqu.roy.historyStorage.Node;
+import com.cqu.roy.historyStorage.VersionTree;
+import com.cqu.roy.mainframe.MainFrame;
 import com.cqu.roy.mywdiget.JpathButton;
 import com.cqu.roy.mywdiget.MainJpanel;
+import com.cqu.roy.mywdiget.MyFontStyle;
+import com.cqu.roy.mywdiget.MyJTextPane;
 
 public class Redo implements FileOperation{
-	private Stack<ArrayList<Integer>> RedoStack;
-	
+	private Stack<HashSet<Integer>> RedoStack;
+	private Stack<HashSet<Integer>> UndoStack;
+	private MainFrame mainFrame;
+	private MyJTextPane jtp;
+	private VersionTree vst;
+	private ArrayList<Node> currentNodeSet;
 	public Redo() {
 		// TODO Auto-generated constructor stub
 		RedoStack = new Stack<>();
@@ -26,6 +40,59 @@ public class Redo implements FileOperation{
 			HashMap<String, MainJpanel> hmTextArea, HashMap<String, TextAtrr> hm_name_atrr,
 			HashMap<String, JpathButton> hm_name_btn) {
 		// TODO Auto-generated method stub
+		mainFrame = MainFrame.getInstance();
+		MainJpanel mainJp = hmTextArea.get(mainFrame.getCurrentAreaName());
+		if (mainJp != null) {
+			jtp = mainJp.getTextPane();
+		}else {
+			return;
+		}
+		vst = jtp.getVersionTree();//版本树
+		currentNodeSet = vst.getCurrentNodeSet();//当前版本节点集合
+		RedoStack = jtp.getRedoStack();
+		UndoStack = jtp.getUndoStack();
+		HashSet<Integer> modified = null;
+		if (!RedoStack.isEmpty()) {
+			modified = RedoStack.pop();
+		}else {
+			return;
+		}
+		Iterator<Integer> iterator = modified.iterator();
+		//文本样式
+		StyledDocument document = jtp.getStyledDocument();
+		MyFontStyle myFontStyle = new MyFontStyle(document);
+		document = myFontStyle.getStyleDoc();
+		jtp.setStyledDocument(document);
+		
+		while(iterator.hasNext()){
+			int lineNum = iterator.next();
+			//当前节点
+			Node current_node = currentNodeSet.get(lineNum);
+			//当前节点的子节点
+			Node sub_node = current_node.getSubNode();
+			//若子节点为空，则进行下一个节点
+			if (sub_node == null) {
+				continue;
+			}
+			//当前节点属性
+			int current_startPositon = current_node.getText().getStartPostion();
+			int current_length = current_node.getText().getLength();
+			//其子节点属性
+			int sub_startPosition = sub_node.getText().getStartPostion();
+			String sub_text = sub_node.getText().getText();
+			try {
+				jtp.getDocument().remove(current_startPositon, current_length);
+				jtp.getDocument().insertString(sub_startPosition, sub_text
+						, document.getStyle("Style06"));
+			} catch (BadLocationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			//重新设置节点集合
+			currentNodeSet.set(lineNum, sub_node);
+		}	
+		//将Redo栈中pop出的元素，压栈入Undo栈
+		UndoStack.push(modified);
 		System.out.println("here is Redo");
 	}
 
--- src/com/cqu/roy/editOperation/Undo.java
+++ src/com/cqu/roy/editOperation/Undo.java
@@ -10,6 +10,7 @@ import java.util.Vector;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.text.BadLocationException;
+import javax.swing.text.StyledDocument;
 
 import com.cqu.roy.attribute.TextAtrr;
 import com.cqu.roy.fileOperation.FileOperation;
@@ -19,11 +20,13 @@ import com.cqu.roy.historyStorage.VersionTree;
 import com.cqu.roy.mainframe.MainFrame;
 import com.cqu.roy.mywdiget.JpathButton;
 import com.cqu.roy.mywdiget.MainJpanel;
+import com.cqu.roy.mywdiget.MyFontStyle;
 import com.cqu.roy.mywdiget.MyJTextPane;
 
 //此只是一个undo操作，具体数据结构在jtp里
 public class Undo implements FileOperation{
 	private Stack<HashSet<Integer>> UndoStack;//Undo栈
+	private Stack<HashSet<Integer>> RedoStack;//Redo栈
 	private MainFrame mainFrame;
 	private MyJTextPane jtp;
 	private VersionTree vst;
@@ -35,24 +38,60 @@ public class Undo implements FileOperation{
 			HashMap<String, JpathButton> hm_name_btn) {
 		// TODO Auto-generated method stub
 		mainFrame = MainFrame.getInstance();
-		jtp = hmTextArea.get(mainFrame.getCurrentAreaName()).getTextPane();
+		MainJpanel mainJp = hmTextArea.get(mainFrame.getCurrentAreaName());
+		if (mainJp != null) {
+			jtp = hmTextArea.get(mainFrame.getCurrentAreaName()).getTextPane();
+		}else {
+			return;
+		}
 		vst = jtp.getVersionTree();//获取版本树
 		currentNodeSet = vst.getCurrentNodeSet();//获取当前节点集合
+		//获取Redo和Undo栈
 		UndoStack = jtp.getUndoStack();
-		HashSet<Integer> modified = UndoStack.pop();
+		RedoStack = jtp.getRedoStack();
+		HashSet<Integer> modified = null;
+		if (!UndoStack.isEmpty()) {
+			modified = UndoStack.pop();
+		}else {
+			return;
+		}
 		Iterator<Integer> iterator = modified.iterator();
+		//文本样式
+		StyledDocument document = jtp.getStyledDocument();
+		MyFontStyle myFontStyle = new MyFontStyle(document);
+		document = myFontStyle.getStyleDoc();
+		jtp.setStyledDocument(document);
+		
 		while(iterator.hasNext()){
 			int lineNum = iterator.next();
+			//当前节点
 			Node node = currentNodeSet.get(lineNum);
-			int startPosition = node.getText().getStartPostion();
-			int length = node.getText().getLength();
+			//其父节点
+			Node parentNode = node.getParentNode();
+			//若父节点为null，则继续
+			if (parentNode == null) {
+				continue;
+			}
+			//当前节点属性
+			int node_startPosition = node.getText().getStartPostion();
+			int node_length = node.getText().getLength();
+			//父节点属性
+			int parent_startPosition = parentNode.getText().getStartPostion();
+			String parent_text = parentNode.getText().getText();
+			int parent_length = parentNode.getText().getLength();
 			try {
-				jtp.getDocument().remove(startPosition, length);
+				jtp.getDocument().remove(node_startPosition, node_length);
+				jtp.getDocument().insertString(parent_startPosition, parent_text
+						, document.getStyle("Style06"));
 			} catch (BadLocationException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+			//将当前节点集合赋为前一节点的父节点
+			currentNodeSet.set(lineNum, parentNode);
 		}
+		//将Undo栈中pop的元素，压栈Redo
+		RedoStack.push(modified);
 		System.out.println("here is undo");
 	}
 }
